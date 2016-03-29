9fd6521dc9e5dc2518a436c382c2f7c63354cd3e
andrewllll <andrew.lubinus@gmail.com>
2016-03-29 17:57:53 -0500

    Added some verification so users can't add products

--- app/controllers/AddProduct.java
+++ app/controllers/AddProduct.java
@@ -1,21 +1,38 @@
 package controllers;
 
 import com.avaje.ebean.Ebean;
+import models.User;
 import models.Product;
 import models.forms.ProductForm;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.addproduct;
+import Util.SessionHandling;
+
 
 /**
  * Created by Sharukh on 3/21/16.
  */
 public class AddProduct extends Controller {
 
+
     public Result product()
     {
-        return ok(addproduct.render(Form.form(ProductForm.class)));
+        String userEmail = SessionHandling.getUser();
+
+       User user = Ebean.find(User.class)
+               .where().like("email", userEmail)
+               .findUnique();
+       if(user == null) {
+            return redirect(controllers.routes.Error.error("User not found, please log in and try again"));
+       }
+       if(user.role.equals("manager")  || user.role.equals("admin")) {
+            return ok(addproduct.render(Form.form(ProductForm.class)));
+       } else {
+            return redirect(controllers.routes.Error.error("User " + user.email + " does not have ability to add product!"));
+       }
+       
     }
 
     public Result addProduct()
